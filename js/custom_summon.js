5307e4d29369761cb5f8a7193c7094fd61f5b0d3
John Krull <astrom.flux@gmail.com>
2012-02-18 00:18:17 -0600

    Found a few more optimizations, reducing the code by another 4 lines.

--- js/custom_summon.js
+++ js/custom_summon.js
@@ -66,26 +66,22 @@ $(function() {
 			if(start_year.length > 0) {
 				if(start_month >= 0) {
 					start_month = (start_month.length < 2) ? "0" + start_month : start_month; // Pad with zeros 
-					start_month =  "-" + start_month; 
 				}
 				if(start_day >= 0) {
 					start_day = (start_day.length < 2) ? "0" + start_day : start_day; // Pad with zeros
-					start_day =  "-" + start_day; 
 				}
 				
-				start_range = start_year + start_month + start_day;
+				start_range = start_year + '-' + start_month + '-' + start_day;
 			}
 		
 			if(end_month >= 0) {
 				end_month = (end_month.length < 2) ? "0" + end_month : end_month; // Pad with zeros
-				end_month = "-" + end_month;
 			}
 			if(end_day >= 0) {
 				end_day = (end_day.length < 2) ? "0" + end_day : end_day; // Pad with zeros
-				end_day = "-" + end_day;
 			}
 
-			$("#insert-pubdate").html('&lt;input type="hidden" name="s.rf" value="PublicationDate,' + start_range + ':' + end_year + end_month + end_day + '" /><br />');
+			$("#insert-pubdate").html('&lt;input type="hidden" name="s.rf" value="PublicationDate,' + start_range + ':' + end_year + '-' + end_month + '-' + end_day + '" /><br />');
 		
 		}
 
