cca8c7061bbec77339729a1c1bc0eba4c91649e0
John Krull <astrom.flux@gmail.com>
2012-02-20 23:14:55 -0600

    Fixed all known bugs and removed 6 more lines of code.

--- js/custom_summon.js
+++ js/custom_summon.js
@@ -34,7 +34,7 @@ $(function() {
 
 		if ($(this).attr('checked')) {
 
-			$("#search-refinements").append('&lt;' + textInput + '&gt;<br />');
+			$("#search-refinements").append('&lt;' + textInput + '&gt;<br/>');
 
 		} else { // Unchecked
 
@@ -50,42 +50,36 @@ $(function() {
 	
 	
 	// Date filter
+	$("#start_year, #end_year").keyup(refreshDateRange);
+	$("#start_month, #start_day, #end_month, #end_day").change(refreshDateRange);
 	
-	$("#end_year").keyup(function() {
+	function refreshDateRange() {
+
+		var start_year = $("#start_year").val();
+		var end_year   = $("#end_year").val();
 		
-		if(this.value.length > 3) {
-			
+		if(end_year.length > 3) {
+		
 			var start_range = '*';
-			var start_year  = $("#start_year").val();
 			var start_month = parseInt($("#start_month").val()) || '';
 			var start_day   = parseInt($("#start_day").val()) || '';
-			var end_year    = $("#end_year").val();
 			var end_month   = parseInt($("#end_month").val()) || '';
 			var end_day     = parseInt($("#end_day").val()) || '';
 
-			if(start_year.length > 0) {
-				if(start_month >= 0) {
-					start_month = (start_month.length < 2) ? "0" + start_month : start_month; // Pad with zeros 
-				}
-				if(start_day >= 0) {
-					start_day = (start_day.length < 2) ? "0" + start_day : start_day; // Pad with zeros
-				}
-				
-				start_range = start_year + '-' + start_month + '-' + start_day;
-			}
+			if(start_month > 0) { start_month = '-' + (start_month.length < 2 ? "0" + start_month : start_month); } // Pad with zeros 
+			if(start_day > 0)   { start_day = '-' + (start_day.length < 2 ? "0" + start_day : start_day);         } // Pad with zeros
+			if(start_year > 0)  { start_range = start_year + start_month + start_day; }
 		
-			if(end_month >= 0) {
-				end_month = (end_month.length < 2) ? "0" + end_month : end_month; // Pad with zeros
-			}
-			if(end_day >= 0) {
-				end_day = (end_day.length < 2) ? "0" + end_day : end_day; // Pad with zeros
-			}
+			if(end_month > 0) { end_month = '-' + (end_month.length < 2 ? "0" + end_month : end_month);           } // Pad with zeros
+			if(end_day > 0)   {	end_day = '-' + (end_day.length < 2 ? "0" + end_day : end_day);                   } // Pad with zeros
 
-			$("#insert-pubdate").html('&lt;input type="hidden" name="s.rf" value="PublicationDate,' + start_range + ':' + end_year + '-' + end_month + '-' + end_day + '" /><br />');
-		
+			end_range = end_year + end_month + end_day;
+			
+			$("#insert-pubdate").html('&lt;input type="hidden" name="s.rf" value="PublicationDate,' + start_range + ':' + end_range + '" /&gt;<br />');
+			$("#search-refinements").html( $("#search-refinements").html() ); // Bug fix for Chrome
 		}
 
-	});
+	}
 	
 	$("#keywords").keyup(function() {
 		var subject_terms = '&lt;input type="hidden" name="s.fvgf[]" value="SubjectTerms,or,' + this.value + '" /&gt;<br />';
