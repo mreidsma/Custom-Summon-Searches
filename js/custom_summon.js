5a007a5a48d71427d639a8b8f15b030b65f1a4d3
John Krull <astrom.flux@gmail.com>
2012-07-04 10:33:47 -0400

    Removed 12 more lines of code and fixed some issues reported by JSLint.

--- js/custom_summon.js
+++ js/custom_summon.js
@@ -7,26 +7,8 @@
 	//
 
 $(function() {
-	
-	function selectText(element) {
-	    var text = document.getElementById(element);    
-
-	    if (document.body.createTextRange) { // ms
-	        var range = document.body.createTextRange();
-	        range.moveToElementText(text);
-	        range.select();
-	    } else if (window.getSelection) { // moz, opera, webkit
-	        var selection = window.getSelection();            
-	        var range = document.createRange();
-	        range.selectNodeContents(text);
-	        selection.removeAllRanges();
-	        selection.addRange(range);
-	    }
-	}
-	
 
 	// Handle the checkboxes
-
 	$(":checkbox").click(function() {
 
 		var data = this.value.split("|");
@@ -43,11 +25,9 @@ $(function() {
 			value = value.replace(/</g, "&lt;").replace(/>/g, "&gt;");
 			value = value.replace(/&gt;/g,"&gt;<br />");
 			$("#search-refinements").html(value);
-			
+            
 		}
-
 	});
-	
 	
 	// Date filter
 	$("#start_year, #end_year").keyup(refreshDateRange);
@@ -59,12 +39,12 @@ $(function() {
 		var end_year   = $("#end_year").val();
 		
 		if(end_year.length > 3) {
-		
+            
 			var start_range = '*';
-			var start_month = parseInt($("#start_month").val()) || '';
-			var start_day   = parseInt($("#start_day").val()) || '';
-			var end_month   = parseInt($("#end_month").val()) || '';
-			var end_day     = parseInt($("#end_day").val()) || '';
+			var start_month = parseInt($("#start_month").val(), 10) || '';
+			var start_day   = parseInt($("#start_day").val(), 10)   || '';
+			var end_month   = parseInt($("#end_month").val(), 10)   || '';
+			var end_day     = parseInt($("#end_day").val(), 10)     || '';
 
 			if(start_month > 0) { start_month = '-' + (start_month.length < 2 ? "0" + start_month : start_month); } // Pad with zeros 
 			if(start_day > 0)   { start_day = '-' + (start_day.length < 2 ? "0" + start_day : start_day);         } // Pad with zeros
@@ -73,38 +53,46 @@ $(function() {
 			if(end_month > 0) { end_month = '-' + (end_month.length < 2 ? "0" + end_month : end_month);           } // Pad with zeros
 			if(end_day > 0)   {	end_day = '-' + (end_day.length < 2 ? "0" + end_day : end_day);                   } // Pad with zeros
 
-			end_range = end_year + end_month + end_day;
+			var end_range = end_year + end_month + end_day;
 			
 			$("#insert-pubdate").html('&lt;input type="hidden" name="s.rf" value="PublicationDate,' + start_range + ':' + end_range + '" /&gt;<br />');
 			$("#search-refinements").html( $("#search-refinements").html() ); // Bug fix for Chrome
 		}
-
 	}
 	
 	$("#keywords").keyup(function() {
-		var subject_terms = '&lt;input type="hidden" name="s.fvgf[]" value="SubjectTerms,or,' + this.value + '" /&gt;<br />';
-		$("#insertterms").html(this.value ? subject_terms : '');
+		$("#insertterms").html(this.value ? '&lt;input type="hidden" name="s.fvgf[]" value="SubjectTerms,or,' + this.value + '" /&gt;<br />' : '');
 	});
 	
 	$(".accordian-body").hide();
 	
 	$(".accordian-head").find("h2").prepend("<span>&#9656;</span>");
-	
 	$(".accordian-head").click(function() {
-		var arrow = $(this).find("h2 span").html();
+        var arrowEl = $(this).find("h2 span span");
 		$(this).next(".accordian-body").slideToggle(400);
-		if(arrow == "<span>▾</span>") {
-			$(this).find("h2 span").html("<span>&#9656;</span>");
+		if(arrowEl.html() == "▾") {
+			arrowEl.html("&#9656;");
 		} else {
-			$(this).find("h2 span").html("<span>&#9662;</span>");
+			arrowEl.html("&#9662;");
 		}
 	});
 	
 	$("#codeblock").prepend('<div class="copybutton lib-button-small-grey">Select All</div>');
 	
-	$(".copybutton").click(function(){
-	    selectText("summon-code");
-	  });
+	$(".copybutton").click(function() {
+        var text = document.getElementById("summon-code");
+        var range = (document.createRange ? document.createRange() : document.body.createTextRange());
+
+        if (document.body.createTextRange) { // ms
+            range.moveToElementText(text);
+            range.select();
+        } else if (window.getSelection) { // moz, opera, webkit
+            var selection = window.getSelection();
+            range.selectNodeContents(text);
+            selection.removeAllRanges();
+            selection.addRange(range);
+        }
+	});
 	
 	$("#clear-dates-button").click(function() {
 		$("#insert-pubdate").html("");
