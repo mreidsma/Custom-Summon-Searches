ec783a3ae34bf1a6ba0af53f1da07e72989a1e10
John Krull <astrom.flux@gmail.com>
2012-07-13 00:45:13 -0400

    Fixed date problem and reduced line count to one hundred.

--- js/custom_summon.js
+++ js/custom_summon.js
@@ -1,6 +1,6 @@
 // Custom Summon Boxes
-	// Grand Valley State University Library Labs, 2011, 2012
-	// by Matthew Reidsma, reidsmam@gvsu.edu with help from John Krull, astrom.flux@gmail.com
+    // Grand Valley State University Library Labs, 2011, 2012
+    // by Matthew Reidsma, reidsmam@gvsu.edu with help from John Krull, astrom.flux@gmail.com
 	// 
 	// Released under Creative Commons Attribution 3.0 Unported License
 	// http://creativecommons.org/licenses/by/3.0/
@@ -35,29 +35,27 @@ $(function() {
 	
 	function refreshDateRange() {
 
+        var full_range = '';
 		var start_year = $("#start_year").val();
 		var end_year   = $("#end_year").val();
 		
-		if(end_year.length > 3) {
-            
-			var start_range = '*';
-			var start_month = parseInt($("#start_month").val(), 10) || '';
-			var start_day   = parseInt($("#start_day").val(), 10)   || '';
-			var end_month   = parseInt($("#end_month").val(), 10)   || '';
-			var end_day     = parseInt($("#end_day").val(), 10)     || '';
+		var start_month = parseInt($("#start_month").val(), 10) || '';
+		var start_day   = parseInt($("#start_day").val(), 10)   || '';
+		var end_month   = parseInt($("#end_month").val(), 10)   || '';
+		var end_day     = parseInt($("#end_day").val(), 10)     || '';
 
-			if(start_month > 0) { start_month = '-' + (start_month.length < 2 ? "0" + start_month : start_month); } // Pad with zeros 
-			if(start_day > 0)   { start_day = '-' + (start_day.length < 2 ? "0" + start_day : start_day);         } // Pad with zeros
-			if(start_year > 0)  { start_range = start_year + start_month + start_day; }
+		if(start_month > 0) { start_month = '-' + (start_month.length < 2 ? "0" + start_month : start_month); } // Pad with zeros 
+		if(start_day > 0)   { start_day = '-' + (start_day.length < 2 ? "0" + start_day : start_day);         } // Pad with zeros
+		if(end_month > 0) { end_month = '-' + (end_month.length < 2 ? "0" + end_month : end_month);           } // Pad with zeros
+		if(end_day > 0)   {	end_day = '-' + (end_day.length < 2 ? "0" + end_day : end_day);                   } // Pad with zeros
+
+        var start_range = start_year ? (start_year + (start_month && start_day ? start_month + start_day : '')) : '*';
+        var end_range   = end_year   ? (end_year   + (end_month   && end_day   ? end_month   + end_day   : '')) : '*';
+        
+        if(start_range == '*' && end_range == '*') { full_range = ''; } else { full_range = start_range + ':' + end_range; }
 		
-			if(end_month > 0) { end_month = '-' + (end_month.length < 2 ? "0" + end_month : end_month);           } // Pad with zeros
-			if(end_day > 0)   {	end_day = '-' + (end_day.length < 2 ? "0" + end_day : end_day);                   } // Pad with zeros
-
-			var end_range = end_year + end_month + end_day;
-			
-			$("#insert-pubdate").html('&lt;input type="hidden" name="s.rf" value="PublicationDate,' + start_range + ':' + end_range + '" /&gt;<br />');
-			$("#search-refinements").html( $("#search-refinements").html() ); // Bug fix for Chrome
-		}
+		$("#insert-pubdate").html(full_range ? '&lt;input type="hidden" name="s.rf" value="PublicationDate,' + full_range + '" /&gt;<br />' : '');
+		$("#search-refinements").html( $("#search-refinements").html() ); // Bug fix for Chrome
 	}
 	
 	$("#keywords").keyup(function() {
@@ -70,7 +68,7 @@ $(function() {
 	$(".accordian-head").click(function() {
         var arrowEl = $(this).find("h2 span span");
 		$(this).next(".accordian-body").slideToggle(400);
-		if(arrowEl.html() == "▾") {
+		if(arrowEl.html() == "?") {
 			arrowEl.html("&#9656;");
 		} else {
 			arrowEl.html("&#9662;");
